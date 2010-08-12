1a9a9623018972480b255fc18e7de9818c4c1e13
Anton Belevich <abelevich@exadel.com>
2010-08-12 14:52:39 +0000

    input demo initial commit

---
+++ input-demo/src/main/java/org/richfaces/demo/SkinBean.java
@@ -0,0 +1,21 @@
+package org.richfaces.demo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "skinBean")
+@SessionScoped
+public class SkinBean {
+    private String skin = "blueSky";
+    
+    public SkinBean() {
+        skin = "blueSky";
+    }
+    public String getSkin() {
+        return skin;
+    }
+
+    public void setSkin(String skin) {
+        this.skin = skin;
+    }
+}
