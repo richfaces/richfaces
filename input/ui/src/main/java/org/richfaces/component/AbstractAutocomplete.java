7e742c7dd76a5af3b645fb1510857cfb7ff5d495
Andrey Markhel <amarkhel@exadel.com>
2010-08-23 15:08:36 +0000

    Rename css classes

--- input/ui/src/main/java/org/richfaces/component/AbstractAutocomplete.java
+++ input/ui/src/main/java/org/richfaces/component/AbstractAutocomplete.java
@@ -82,7 +82,7 @@ public abstract class AbstractAutocomplete extends UIInput implements MetaCompon
     @Attribute
     public abstract String getFilterFunction();
 	
-    @Attribute(defaultValue = "rf-au-s")
+    @Attribute(defaultValue = "rf-au-select")
     public abstract String getSelectedItemClass();
 
     @Attribute
