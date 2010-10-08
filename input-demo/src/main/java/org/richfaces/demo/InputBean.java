48c4e0a96c617d03e7a2025b7aef751d198d5259
Anton Belevich <abelevich@exadel.com>
2010-10-08 13:08:18 +0000

    inplaceSelect developer sample

---
+++ input-demo/src/main/java/org/richfaces/demo/InputBean.java
@@ -0,0 +1,20 @@
+package org.richfaces.demo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@SessionScoped
+@ManagedBean(name = "inputBean")
+public class InputBean {
+
+    private String value;
+    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
