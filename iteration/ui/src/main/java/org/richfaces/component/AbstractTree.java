ec294c4e25f56c8b8bd5c0d0014c72e31f9fce30
Nick Belaevski <nbelaevski@exadel.com>
2010-11-18 16:28:44 +0000

    https://jira.jboss.org/browse/RF-9716

--- iteration/ui/src/main/java/org/richfaces/component/AbstractTree.java
+++ iteration/ui/src/main/java/org/richfaces/component/AbstractTree.java
@@ -51,6 +51,8 @@ import org.richfaces.context.ExtendedVisitContextMode;
 import org.richfaces.convert.SequenceRowKeyConverter;
 import org.richfaces.event.TreeSelectionEvent;
 import org.richfaces.event.TreeSelectionListener;
+import org.richfaces.log.Logger;
+import org.richfaces.log.RichfacesLogger;
 import org.richfaces.model.ExtendedTreeDataModelImpl;
 import org.richfaces.model.SwingTreeNodeDataModelImpl;
 import org.richfaces.model.TreeDataModel;
