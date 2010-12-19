5f6f38cca290c4bce076e4b7a3b3281594e9c326
Alexander Smirnov <asmirnov@exadel.com>
2010-12-19 22:05:58 +0000

    Merged revisions 20630-20632,20646,20657,20674,20676-20677 via svnmerge from https://svn.jboss.org/repos/richfaces/branches/RF-7654
    ........
      r20630 | alexsmirnov | 2010-12-16 17:09:52 -0800 (Thu, 16 Dec 2010) | 2 lines
      
      CODING IN PROGRESS - issue RF-7654: rich:message/rich:messages 
      https://issues.jboss.org/browse/RF-7654
    ........
      r20631 | alexsmirnov | 2010-12-16 17:21:37 -0800 (Thu, 16 Dec 2010) | 2 lines
      
      CODING IN PROGRESS - issue RF-7654: rich:message/rich:messages 
      https://issues.jboss.org/browse/RF-7654
    ........
      r20632 | alexsmirnov | 2010-12-16 17:27:58 -0800 (Thu, 16 Dec 2010) | 2 lines
      
      CODING IN PROGRESS - issue RF-7654: rich:message/rich:messages 
      https://issues.jboss.org/browse/RF-7654
    ........
      r20646 | nbelaevski | 2010-12-17 06:15:05 -0800 (Fri, 17 Dec 2010) | 1 line
      
      Removed duplicate files
    ........
      r20657 | pyaschenko | 2010-12-17 07:59:45 -0800 (Fri, 17 Dec 2010) | 1 line
      
      http://jira.jboss.com/jira/browse/RF-9250
    ........
      r20674 | alexsmirnov | 2010-12-17 16:11:17 -0800 (Fri, 17 Dec 2010) | 2 lines
      
      CODING IN PROGRESS - issue RF-7654: rich:message/rich:messages 
      https://issues.jboss.org/browse/RF-7654
    ........
      r20676 | alexsmirnov | 2010-12-17 16:39:32 -0800 (Fri, 17 Dec 2010) | 2 lines
      
      CODING IN PROGRESS - issue RF-7654: rich:message/rich:messages 
      https://issues.jboss.org/browse/RF-7654
    ........
      r20677 | alexsmirnov | 2010-12-18 22:02:48 -0800 (Sat, 18 Dec 2010) | 2 lines
      
      RESOLVED - issue RF-9222: CSV: development sample subtask 
      https://issues.jboss.org/browse/RF-9222
    ........

--- validator-demo/src/main/java/org/richfaces/example/Bean.java
+++ validator-demo/src/main/java/org/richfaces/example/Bean.java
@@ -1,70 +1,22 @@
-/**
- * License Agreement.
- *
- * Rich Faces - Natural Ajax for Java Server Faces (JSF)
- *
- * Copyright (C) 2007 Exadel, Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- */
-
 package org.richfaces.example;
 
-import javax.validation.constraints.Pattern;
-
-
-/**
- * JSF bean with text property validation.
- */
 public class Bean {
-	
-	/**
-	 * Text property
-	 */
-	private String email;
-	
-	private String creditCardNumber;
+    
+    public static final String FOO_VALUE = "fooValue";
 
-	/**
-	 * @return the creditCardNumber
-	 */
-	@Pattern(regexp="\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d\\\\d")
-	public String getCreditCardNumber() {
-		return creditCardNumber;
-	}
+    private String value=FOO_VALUE;
 
-	/**
-	 * @param creditCardNumber the creditCardNumber to set
-	 */
-	public void setCreditCardNumber(String creditCardNumber) {
-		this.creditCardNumber = creditCardNumber;
-	}
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+    /**
+     * @param value the value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
 
-	/**
-	 * @return the text
-	 */
-//	@Email
-//	@Pattern(regexp="^[a-zA-Z][\\w\\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\\w\\.-]*[a-zA-Z0-9]\\.[a-zA-Z][a-zA-Z\\.]*[a-zA-Z]$")
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param text the text to set
-	 */
-	public void setEmail(String text) {
-		this.email = text;
-	}
-	
 }
