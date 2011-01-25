60035465bcfe11a9d0bef5f970efba8094421862
Nick Belaevski <nbelaevski@exadel.com>
2011-01-25 12:31:56 +0000

    Panel menu icons refactoring

---
+++ common/ui/src/main/java/org/richfaces/renderkit/html/iconimages/PanelIconSpacer.java
@@ -0,0 +1,35 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces - Ajax4jsf Component Library
+ *
+ * Copyright (C) 2007  Exadel, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+package org.richfaces.renderkit.html.iconimages;
+
+import org.richfaces.resource.DynamicUserResource;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+@DynamicUserResource
+public class PanelIconSpacer extends PanelIconBasic {
+
+    protected void paintImage(Graphics2D g2d, Color color) {
+    }
+
+}
