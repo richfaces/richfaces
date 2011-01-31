5a9e8dea83c429a3cd5637109f8c98416a75b685
Ilya Shaikovsky <ishaikovsky@exadel.com>
2011-01-31 11:35:34 +0000

    https://issues.jboss.org/browse/RF-9257

---
+++ src/main/java/org/richfaces/demo/validation/DayStatistics.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.richfaces.demo.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Ilya Shaikovsky
+ * 
+ */
+@ManagedBean
+@RequestScoped
+public class DayStatistics {
+
+	public DayStatistics() {
+		dayPasstimes.add(new PassTime("Sport", 0));
+		dayPasstimes.add(new PassTime("Entertainment", 0));
+		dayPasstimes.add(new PassTime("Sleeping", 0));
+		dayPasstimes.add(new PassTime("Games", 0));
+	}
+
+	private List<PassTime> dayPasstimes = new ArrayList<PassTime>();
+
+	public List<PassTime> getDayPasstimes() {
+		return dayPasstimes;
+	}
+
+	public void setDayPasstimes(List<PassTime> dayPasstimes) {
+		this.dayPasstimes = dayPasstimes;
+	}
+
+	@NotNull
+	@Min(value = 1, message = "Please feel at list one entry")
+	@Max(value = 24, message = "Only 24h in a day!")
+	public Integer getTotalTime() {
+		Integer result = new Integer(0);
+		for (PassTime passtime : dayPasstimes) {
+			result += passtime.getTime();
+		}
+		return result;
+	}
+
+	public void store(ActionEvent event) {
+		FacesContext.getCurrentInstance().addMessage(
+				event.getComponent().getClientId(
+						FacesContext.getCurrentInstance()),
+				new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Changes Stored Successfully",
+						"Changes Stored Successfully"));
+	}
+}
---
+++ src/main/java/org/richfaces/demo/validation/PassTime.java
@@ -0,0 +1,37 @@
+package org.richfaces.demo.validation;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class PassTime {
+
+	public PassTime(String title, Integer time) {
+		setTitle(title);
+		setTime(time);
+	}
+	
+	@NotEmpty
+	@Length(max=15, min=3)
+	private String title;
+	@NotNull
+	@Min(0)
+	@Max(12)
+	private Integer time;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getTime() {
+		return time;
+	}
+	public void setTime(Integer time) {
+		this.time = time;
+	}
+	
+}
