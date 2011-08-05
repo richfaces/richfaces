1821ded5d40aa516c3cd72ab5df1b16d54f4ed57
Lukas Fryc <lfryc@redhat.com>
2011-08-05 18:25:37 +0200

    fixed script evaluation for script for_each_module.sh

--- scripts/for_each_module.sh
+++ scripts/for_each_module.sh
@@ -2,7 +2,7 @@
 
 MODULES="archetypes build cdk components core dev-examples showcase"
 
-EXECUTE=$*
+EXECUTE="$@"
 
 SCRIPTS=`dirname $0`
 TOPDIR=`readlink -f $SCRIPTS/../..`
@@ -10,6 +10,6 @@ TOPDIR=`readlink -f $SCRIPTS/../..`
 for MODULE in $MODULES; do
 	pushd "$TOPDIR/$MODULE" >/dev/null
 		echo "Running '$EXECUTE' on module '$MODULE'"
-		$EXECUTE
+		eval "$EXECUTE"
 	popd >/dev/null
 done
