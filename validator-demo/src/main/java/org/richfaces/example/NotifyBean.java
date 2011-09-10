615ffd6bfa6e974f7318c89f4d3c3dd2e43a4b1e
Lukas Fryc <lfryc@redhat.com>
2011-09-10 16:38:31 +0200

    Notify refactored (RF-11182)

--- validator-demo/src/main/java/org/richfaces/example/NotifyBean.java
+++ validator-demo/src/main/java/org/richfaces/example/NotifyBean.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.richfaces;
+package org.richfaces.example;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
