7f3d54b7b266f3dc34b85bc432d2c7c954bce3e4
Lukas Fryc <lfryc@redhat.com>
2011-10-12 15:40:26 +0200

    RF-11516: Static resource mapping handling absolute URLs

---
+++ impl/src/main/java/org/richfaces/util/URLUtils.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Utilities for handling {@link URL} instances.
+ *
+ * @author <a href="mailto:lfryc@redhat.com">Lukas Fryc</a>
+ */
+public class URLUtils {
+
+    private URLUtils() {
+    }
+
+    /**
+     * <p>
+     * Returns true if the URL given as string is valid URL.
+     * </p>
+     *
+     * <p>
+     * Specifically it verifies that all mandatory parts of URL are in place and all other parts have valid form.
+     * </p>
+     *
+     * @param url the URL as string to be verified for valid URL form
+     * @return Returns true if the URL given as string is valid URL
+     */
+    public static boolean isValidURL(String url) {
+        try {
+            new URL(url);
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        }
+    }
+}
