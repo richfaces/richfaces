379d808eae07a86304748f96efd6dbeaefbe4fbc
Wesley Hales <wesleyhales@gmail.com>
2011-11-10 13:46:04 -0500

    the home for mobile helper code for RichFaces 4.1.0

--- mobile-compatibility/slidfast.js
+++ mobile-compatibility/slidfast.js
@@ -102,10 +102,8 @@ var slidfast = {
             if (hashArray.length === 2) {
                 id = hashArray[0].replace('#', '');
                 sample = hashArray[1];
-                //todo - check for method first
                 handleHashChange(id, sample);
                 //show the back button and attach functions
-                //todo fire event and handle this on client side
                 document.getElementById("back-button").className = 'basic-button left-header-button';
                 document.getElementById("back-button").onclick = function() {
                     slidfast.slideTo(slidfast.defaultPageID);
@@ -137,53 +135,4 @@ var slidfast = {
         }
     }
 
-};
-
-function EventTarget(){
-    this._listeners = {};
-}
-
-EventTarget.prototype = {
-
-    constructor: EventTarget,
-
-    addListener: function(type, listener){
-        if (typeof this._listeners[type] == "undefined"){
-            this._listeners[type] = [];
-        }
-
-        this._listeners[type].push(listener);
-    },
-
-    fire: function(event){
-        if (typeof event == "string"){
-            event = { type: event };
-        }
-        if (!event.target){
-            event.target = this;
-        }
-
-        if (!event.type){  //falsy
-            throw new Error("Event object missing 'type' property.");
-        }
-
-        if (this._listeners[event.type] instanceof Array){
-            var listeners = this._listeners[event.type];
-            for (var i=0, len=listeners.length; i < len; i++){
-                listeners[i].call(this, event);
-            }
-        }
-    },
-
-    removeListener: function(type, listener){
-        if (this._listeners[type] instanceof Array){
-            var listeners = this._listeners[type];
-            for (var i=0, len=listeners.length; i < len; i++){
-                if (listeners[i] === listener){
-                    listeners.splice(i, 1);
-                    break;
-                }
-            }
-        }
-    }
 };
