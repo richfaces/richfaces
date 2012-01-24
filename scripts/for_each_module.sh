3d8ec29747f02d0e77681d3af61d404d748ce7da
Brian Leathem <bleathem@gmail.com>
2012-01-24 12:02:00 -0800

    Added a font color setting to indicate module boundaries in the for_each shell script

--- scripts/for_each_module.sh
+++ scripts/for_each_module.sh
@@ -9,7 +9,9 @@ TOPDIR=`readlink -f $SCRIPTS/../..`
 
 for MODULE in $MODULES; do
 	pushd "$TOPDIR/$MODULE" >/dev/null
+	        tput setaf 3
 		echo "Running '$EXECUTE' on module '$MODULE'"
+	        tput setaf 7
 		eval "$EXECUTE"
 	popd >/dev/null
 done
