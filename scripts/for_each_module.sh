2a089f6fe705cdd45245510a8113301234ace343
Lukas Fryc <lfryc@redhat.com>
2012-01-25 14:08:24 +0100

    for_each_module.sh: tweaked colors to work on light background; added horizontal rulers

--- scripts/for_each_module.sh
+++ scripts/for_each_module.sh
@@ -7,11 +7,19 @@ EXECUTE="$@"
 SCRIPTS=`dirname $0`
 TOPDIR=`readlink -f $SCRIPTS/../..`
 
+#Colors
+Brown="$(tput setaf 3)"
+Green="$(tput setaf 2)"
+NoColor="$(tput sgr0)"
+Ruler="$Green################################################$NoColor"
+
+echo $Ruler
 for MODULE in $MODULES; do
 	pushd "$TOPDIR/$MODULE" >/dev/null
-	        tput setaf 3
-		echo "Running '$EXECUTE' on module '$MODULE'"
-	        tput setaf 7
+		echo -n $Brown
+		echo "[ $MODULE ] > $EXECUTE"
+	        echo -n $NoColor
 		eval "$EXECUTE"
 	popd >/dev/null
 done
+echo $Ruler
