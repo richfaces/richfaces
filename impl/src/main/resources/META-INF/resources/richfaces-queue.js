d1e9702a4efb66609b073987104a540f1a8e4867
Brian Leathem <bleathem@gmail.com>
2012-01-27 12:43:52 -0800

    RF-11838: Removed the layerX and layerY events (generated in WebKit browsers)

--- impl/src/main/resources/META-INF/resources/richfaces-queue.js
+++ impl/src/main/resources/META-INF/resources/richfaces-queue.js
@@ -125,6 +125,10 @@
                 this.queueOptions.requestGroupingId = typeof this.source == "string" ? this.source : this.source.id;
             }
 
+            // Remove the layerX and layerY events (generated in WebKit browsers)
+            delete event.layerX;
+            delete event.layerY;
+
             // copy of event should be created otherwise IE will fail
             this.event = $.extend({}, event);
 
