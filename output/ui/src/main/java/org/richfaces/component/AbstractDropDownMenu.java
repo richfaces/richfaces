20a461760d878e686eb25980cf4920d519806df4
Brian Leathem <bleathem@gmail.com>
2012-02-21 09:30:03 -0800

    RFPL-1959: Attribute descriptions for rich:dropDownMenu

--- output/ui/src/main/java/org/richfaces/component/AbstractDropDownMenu.java
+++ output/ui/src/main/java/org/richfaces/component/AbstractDropDownMenu.java
@@ -12,11 +12,14 @@ import org.richfaces.renderkit.html.DropDownMenuRendererBase;
 
 @JsfComponent(family = AbstractDropDownMenu.COMPONENT_FAMILY, type = AbstractDropDownMenu.COMPONENT_TYPE, facets = {
         @Facet(name = "label", generate = false), @Facet(name = "labelDisabled", generate = false) }, renderer = @JsfRenderer(type = DropDownMenuRendererBase.RENDERER_TYPE), tag = @Tag(name = "dropDownMenu"), attributes = {
-        "events-mouse-props.xml", "events-key-props.xml", "core-props.xml", "i18n-props.xml" })
+        "events-mouse-props.xml", "events-key-props.xml", "core-props.xml", "i18n-props.xml", "position-props.xml" })
 public abstract class AbstractDropDownMenu extends AbstractMenuContainer {
     public static final String COMPONENT_TYPE = "org.richfaces.DropDownMenu";
     public static final String COMPONENT_FAMILY = "org.richfaces.DropDownMenu";
 
+    /**
+     * The text label for the menu item. Alternatively, use the label facet to define content for the label
+     */
     @Attribute
     public abstract String getLabel();
 
