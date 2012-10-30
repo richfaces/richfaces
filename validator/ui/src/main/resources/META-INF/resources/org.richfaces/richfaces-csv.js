cf3c3ba9e6e249f0416e07ee2e5e1eaed7000bbc
Brian Leathem <bleathem@gmail.com>
2012-10-30 16:15:02 -0700

    RF-12583: Fixed the ";" type

--- validator/ui/src/main/resources/META-INF/resources/org.richfaces/richfaces-csv.js
+++ validator/ui/src/main/resources/META-INF/resources/org.richfaces/richfaces-csv.js
@@ -327,7 +327,7 @@
             pattern = '^' + pattern;
         }
         if (! (pattern.slice(-1) === '$') ) {
-            pattern = pattern; + '$';
+            pattern = pattern + '$';
         }
         return pattern;
     }
