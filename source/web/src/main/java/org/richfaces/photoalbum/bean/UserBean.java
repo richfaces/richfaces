fcc94f16f834d7ddc87c42110f62819f47534677
Michal Petrov <mpetrov@redhat.com>
2012-11-07 17:16:11 +0100

    update UserBean for use in security

--- source/web/src/main/java/org/richfaces/photoalbum/bean/UserBean.java
+++ source/web/src/main/java/org/richfaces/photoalbum/bean/UserBean.java
@@ -1,23 +1,26 @@
 package org.richfaces.photoalbum.bean;
 
-import javax.ejb.Singleton;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 
-import org.richfaces.photoalbum.domain.Album;
-import org.richfaces.photoalbum.domain.Image;
-import org.richfaces.photoalbum.domain.Shelf;
+import org.jboss.logging.Logger;
+//import org.richfaces.photoalbum.domain.Album;
+//import org.richfaces.photoalbum.domain.Image;
+//import org.richfaces.photoalbum.domain.Shelf;
 import org.richfaces.photoalbum.domain.User;
 import org.richfaces.photoalbum.service.Constants;
 
 /**
- * This bean was created to provide User entity in places where it is needed (UserAction and ShelfAction).
- * This is a temporary solution.
- *
+ * This bean will work as a part of a simple security checking
+ * 
  * @author mpetrov
  */
 
-@Singleton
+@Named
+@SessionScoped
 public class UserBean {
 
     @Inject
@@ -25,13 +28,28 @@ public class UserBean {
 
     private User user;
 
-    private Shelf currentShelf;
-    private Album currentAlbum;
-    private Image currentImage;
+    // private Shelf currentShelf;
+    // private Album currentAlbum;
+    // private Image currentImage;
 
     private boolean logged;
 
     // -- getters and setters
+
+    private boolean autolog = false; //
+
+    private Logger logger = Logger.getLogger(UserBean.class);
+
+    @PostConstruct
+    private void logUser() {
+        if (autolog) {
+            try {
+                user = logIn("amarkhel", "8cb2237d0679ca88db6464eac60da96345513964");
+            } catch (Exception e) {
+                logger.info("Autolog unsuccessful:" + e.getMessage());
+            }
+        }
+    }
 
     public User logIn(String username, String passwordHash) throws Exception {
         user = (User) em.createNamedQuery(Constants.USER_LOGIN_QUERY).setParameter(Constants.USERNAME_PARAMETER, username)
@@ -50,39 +68,37 @@ public class UserBean {
 
     public void refreshUser() {
         if (logged) {
-            user = (User) em.createNamedQuery(Constants.USER_LOGIN_QUERY).setParameter(Constants.USERNAME_PARAMETER, user.getLogin())
+            user = (User) em.createNamedQuery(Constants.USER_LOGIN_QUERY)
+                .setParameter(Constants.USERNAME_PARAMETER, user.getLogin())
                 .setParameter(Constants.PASSWORD_PARAMETER, user.getPasswordHash()).getSingleResult();
             logged = user != null;
         }
+    }
+    
+    public boolean isLoggedIn() {
+        return logged;
+    }
+
+    public boolean isUserAdmin() {
+        if (user == null) return false;
+        return user.getLogin().equals("amarkhel") || user.getLogin().equals("Viking");
     }
 
     // public void setUser(User user) {
     // this.user = user;
     // }
-    public Shelf getCurrentShelf() {
-        return currentShelf;
-    }
-
-    public void setCurrentShelf(Shelf currentShelf) {
-        this.currentShelf = currentShelf;
-    }
-
-    public Album getCurrentAlbum() {
-        return currentAlbum;
-    }
-
-    public void setCurrentAlbum(Album currentAlbum) {
-        this.currentAlbum = currentAlbum;
-    }
-
-    public Image getCurrentImage() {
-        return currentImage;
-    }
-
-    public void setCurrentImage(Image currentImage) {
-        this.currentImage = currentImage;
-    }
-
+    /*
+     * public Shelf getCurrentShelf() { return currentShelf; }
+     * 
+     * public void setCurrentShelf(Shelf currentShelf) { this.currentShelf = currentShelf; }
+     * 
+     * public Album getCurrentAlbum() { return currentAlbum; }
+     * 
+     * public void setCurrentAlbum(Album currentAlbum) { this.currentAlbum = currentAlbum; }
+     * 
+     * public Image getCurrentImage() { return currentImage; }
+     * 
+     * public void setCurrentImage(Image currentImage) { this.currentImage = currentImage; }
+     */
     // -- management classes
-
 }
