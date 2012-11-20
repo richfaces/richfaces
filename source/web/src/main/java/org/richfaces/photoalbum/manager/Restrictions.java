8942793325f559cf231f5ba63909f0f4abbcd53b
Michal Petrov <mpetrov@redhat.com>
2012-11-20 15:54:41 +0100

    change user role checking

--- source/web/src/main/java/org/richfaces/photoalbum/manager/Restrictions.java
+++ source/web/src/main/java/org/richfaces/photoalbum/manager/Restrictions.java
@@ -1,13 +1,19 @@
 package org.richfaces.photoalbum.manager;
 
+import javax.inject.Inject;
+
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.annotations.Secures;
-import org.richfaces.photoalbum.service.Constants;
+import org.richfaces.photoalbum.bean.UserBean;
 
 public class Restrictions {
+    @Inject
+    UserBean uBean;
+
     public @Secures
     @AdminRestricted
     boolean isAdmin(Identity identity) {
-        return identity.hasRole(Constants.ADMIN_ROLE, "Users", "GROUP");
+        //return identity.hasRole(Constants.ADMIN_ROLE, "Users", "GROUP");
+        return uBean.getUser().getLogin().equals("amarkhel");
     }
 }
