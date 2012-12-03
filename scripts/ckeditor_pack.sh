1b5bec5021fb1b0cd907bad3628f8aa3633c4983
Lukas Fryc <lfryc@redhat.com>
2012-12-03 16:02:22 +0100

    RF-12633: added instructions for upgrading CKEditor

--- scripts/ckeditor_pack.sh
+++ scripts/ckeditor_pack.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# to build CKEditor, you need to
+# 1. download distribution package including _source folder
+# 2. checkout appropriate tag from SVN
+# 3. copy _dev from checkout SVN folder to the contents of distribution package
+# 4. change ckeditor.pack: from 'kama' as default skin to 'richfaces': _source/skins/richfaces/skin.js
+# 5. run _dev/releaser/releaser.sh
+# 6. copy contents of _dev/releaser/release/release to JSF resources: org.richfaces.ckeditor
+# 7. rename and fix generated skins/richfaces/editor.css to editor.ecss
+#     - fix ckeditor.js and skins/richfaces/skin.jsto point to editor.ecss
+#     - fix editor.ecss (ECSS compiler complains) - contains invalid #{richSkin....;} and #{url...;} instructions - remove semi-colon
+#     - fix editor.ecss - some wrong line-endings (the line ends after ' sign)
+# 
 # This script takes all the resources requires by CKEditor to work properly
 # and copy them to ./org.richfaces.ckeditor/ folder.
 #
