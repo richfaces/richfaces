c5f32e62ec734ca94cb225b0a5aac7650a6f6987
Brian Leathem <bleathem@gmail.com>
2013-01-29 23:53:43 -0800

    RF-12761: Test case

--- core/ui/src/test/integration/org/richfaces/component/ajax/AjaxBean.java
+++ core/ui/src/test/integration/org/richfaces/component/ajax/AjaxBean.java
@@ -22,6 +22,9 @@
 package org.richfaces.component.ajax;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -49,5 +52,13 @@ public class AjaxBean implements Serializable {
 
     public void listener(String newValue) {
         this.value = newValue;
+    }
+
+    public void methodA(ActionEvent event) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("AjaxBean.methodA()"));
+    }
+
+    public void methodB(ActionEvent event) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("AjaxBean.methodB()"));
     }
 }
--- core/ui/src/test/integration/org/richfaces/integration/CoreUIDeployment.java
+++ core/ui/src/test/integration/org/richfaces/integration/CoreUIDeployment.java
@@ -1,11 +1,17 @@
 package org.richfaces.integration;
 
+import com.google.common.collect.Sets;
 import org.jboss.shrinkwrap.api.Filters;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.richfaces.deployment.Deployment;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Set;
 
 public class CoreUIDeployment extends Deployment {
 
@@ -24,6 +30,22 @@ public class CoreUIDeployment extends Deployment {
             .importDirectory("target/classes/").as(GenericArchive.class),
             "/", Filters.includeAll());
         archive().addAsLibrary(miscArchive);
-
     }
+
+    public CoreUIDeployment(Class<?> testClass, String version) {
+        super(testClass);
+
+        Set<File> jarFiles = Sets.newHashSet();
+        String[] deps = {
+                "org.richfaces.core:richfaces-core-api:" + version,
+                "org.richfaces.core:richfaces-core-impl:" + version,
+                "org.richfaces.ui:richfaces-components-api:" + version,
+                "org.richfaces.ui:richfaces-components-ui:" + version};
+        for (String dep : deps) {
+            jarFiles.addAll(Arrays.asList(Maven.resolver().resolve(dep)
+                    .withClassPathResolution(false).withTransitivity().asFile()));
+        }
+        archive().addAsLibraries(jarFiles.toArray(new File[jarFiles.size()]));
+    }
+
 }
