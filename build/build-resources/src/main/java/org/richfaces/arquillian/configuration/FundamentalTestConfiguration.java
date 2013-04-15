b8a4ac75a464e803592dede69960e30046ccba8e
Lukas Fryc <lfryc@redhat.com>
2013-04-15 15:05:34 +0200

    working release settings
    Conflicts:
    	build/build-resources/pom.xml
    	examples/photoalbum/pom.xml

--- build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
@@ -10,7 +10,7 @@ public class FundamentalTestConfiguration implements DroneConfiguration<Fundamen
 
     private String richfacesVersion;
     private Boolean servletContainerSetup;
-    private String currentBuildRichfacesVersion = "4.3.1-SNAPSHOT";
+    private String currentBuildRichfacesVersion = "5.0.0-SNAPSHOT";
     private String jsfImplementation;
     private String containerHome;
     private String containerDistribution;
