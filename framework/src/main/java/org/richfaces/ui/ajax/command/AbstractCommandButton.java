3712e2fe5348616e7a049e296deb53c5788af828
Brian Leathem <bleathem@gmail.com>
2013-04-16 10:02:42 -0700

    RF-12846: Added the Accesskey interface, and changed the commandButton and commandLink to implement that interface

--- framework/src/main/java/org/richfaces/ui/ajax/command/AbstractCommandButton.java
+++ framework/src/main/java/org/richfaces/ui/ajax/command/AbstractCommandButton.java
@@ -21,19 +21,20 @@
  */
 package org.richfaces.ui.ajax.command;
 
+import javax.faces.component.UICommand;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
 import org.richfaces.cdk.annotations.Attribute;
 import org.richfaces.cdk.annotations.JsfComponent;
 import org.richfaces.cdk.annotations.JsfRenderer;
 import org.richfaces.cdk.annotations.Tag;
 import org.richfaces.cdk.annotations.TagType;
 import org.richfaces.ui.ajax.region.AjaxContainer;
+import org.richfaces.ui.attribute.AccesskeyProps;
 import org.richfaces.ui.common.AbstractActionComponent;
 import org.richfaces.ui.common.AjaxConstants;
 import org.richfaces.ui.common.meta.MetaComponentResolver;
-
-import javax.faces.component.UICommand;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
 
 /**
  * <p>
@@ -43,8 +44,8 @@ import javax.faces.context.FacesContext;
  * @author Nick Belaevski
  */
 @JsfComponent(renderer = @JsfRenderer(type = "org.richfaces.CommandButtonRenderer"), tag = @Tag(type = TagType.Facelets),
-        attributes = {"ajax-props.xml", "command-button-props.xml", "core-props.xml" })
-public abstract class AbstractCommandButton extends AbstractActionComponent implements MetaComponentResolver {
+        attributes = {"ajax-props.xml", "command-button-props.xml", "core-props.xml", "accesskey-props.xml" })
+public abstract class AbstractCommandButton extends AbstractActionComponent implements MetaComponentResolver, AccesskeyProps {
     public static final String COMPONENT_TYPE = "org.richfaces.CommandButton";
     public static final String COMPONENT_FAMILY = UICommand.COMPONENT_FAMILY;
 
