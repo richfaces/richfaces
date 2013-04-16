a50fd863cfd8a09bf043a5ece4adbed8066ebcc0
Michal Petrov <mpetrov@redhat.com>
2013-04-16 13:23:24 +0200

    add event managing

---
+++ examples/photoalbum/src/main/java/org/richfaces/photoalbum/service/IEventAction.java
@@ -0,0 +1,28 @@
+package org.richfaces.photoalbum.service;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import org.richfaces.photoalbum.domain.Event;
+import org.richfaces.photoalbum.domain.EventCategory;
+
+@Local
+public interface IEventAction {
+
+    void addEvent(Event event) throws PhotoAlbumException;
+
+    void deleteEvent(Event event) throws PhotoAlbumException;
+
+    void editEvent(Event event) throws PhotoAlbumException;
+
+    void resetEvent(Event event);
+    
+    List<Event> getAllEvents();
+    
+    List<EventCategory> getEventCategories();
+    
+    Event getEventById(long id);
+    
+    EventCategory getEventCategoryById(long id);
+}
