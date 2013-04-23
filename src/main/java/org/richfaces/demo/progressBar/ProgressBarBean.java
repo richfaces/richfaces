69d81fa0726ee1e4a61b4a0e2ee57e8173e99ba7
Michal Petrov <mpetrov@redhat.com>
2013-04-23 16:31:38 +0200

    change increment implementation

--- src/main/java/org/richfaces/demo/progressBar/ProgressBarBean.java
+++ src/main/java/org/richfaces/demo/progressBar/ProgressBarBean.java
@@ -4,7 +4,6 @@
 package org.richfaces.demo.progressBar;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -18,29 +17,21 @@ public class ProgressBarBean implements Serializable {
     private static final long serialVersionUID = -314414475508376585L;
     private boolean buttonRendered = true;
     private boolean enabled = false;
-    private Long startTime;
+    private int currentValue;
 
     public String startProcess() {
         setEnabled(true);
         setButtonRendered(false);
-        setStartTime(new Date().getTime());
+        setCurrentValue(0);
         return null;
     }
 
-    public Long getCurrentValue() {
-        if (isEnabled()) {
-            Long current = (new Date().getTime() - startTime) / 1000;
-            if (current >= 100) {
+    public void increment() {
+        if (isEnabled() && currentValue < 100) {
+            currentValue += 2;
+            if (currentValue >= 100) {
                 setButtonRendered(true);
-            } else if (current.equals(0)) {
-                return new Long(1);
             }
-            return (new Date().getTime() - startTime) / 1000;
-        }
-        if (startTime == null) {
-            return Long.valueOf(-1);
-        } else {
-            return Long.valueOf(100);
         }
     }
 
@@ -52,14 +43,6 @@ public class ProgressBarBean implements Serializable {
         this.enabled = enabled;
     }
 
-    public Long getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Long startTime) {
-        this.startTime = startTime;
-    }
-
     public boolean isButtonRendered() {
         return buttonRendered;
     }
@@ -67,4 +50,16 @@ public class ProgressBarBean implements Serializable {
     public void setButtonRendered(boolean buttonRendered) {
         this.buttonRendered = buttonRendered;
     }
+
+    public int getCurrentValue() {
+        if (!isEnabled()) {
+            return -1;
+        }
+        return currentValue;
+    }
+
+    public void setCurrentValue(int currentValue) {
+        this.currentValue = currentValue;
+    }
+
 }
