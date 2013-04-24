bbcb8624114b5bb3027e9e1d5ded9c0d3fc3b857
Michal Petrov <mpetrov@redhat.com>
2013-04-24 09:21:47 +0200

    RF-12941: ProgressBar demo improvements

--- examples/showcase/src/main/java/org/richfaces/demo/progressBar/ProgressBarBean.java
+++ examples/showcase/src/main/java/org/richfaces/demo/progressBar/ProgressBarBean.java
@@ -1,32 +1,9 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
-
 /**
  *
  */
 package org.richfaces.demo.progressBar;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -40,29 +17,21 @@ public class ProgressBarBean implements Serializable {
     private static final long serialVersionUID = -314414475508376585L;
     private boolean buttonRendered = true;
     private boolean enabled = false;
-    private Long startTime;
+    private int currentValue;
 
     public String startProcess() {
         setEnabled(true);
         setButtonRendered(false);
-        setStartTime(new Date().getTime());
+        setCurrentValue(0);
         return null;
     }
 
-    public Long getCurrentValue() {
-        if (isEnabled()) {
-            Long current = (new Date().getTime() - startTime) / 1000;
-            if (current >= 100) {
+    public void increment() {
+        if (isEnabled() && currentValue < 100) {
+            currentValue += 2;
+            if (currentValue >= 100) {
                 setButtonRendered(true);
-            } else if (current.equals(0)) {
-                return new Long(1);
             }
-            return (new Date().getTime() - startTime) / 1000;
-        }
-        if (startTime == null) {
-            return Long.valueOf(-1);
-        } else {
-            return Long.valueOf(100);
         }
     }
 
@@ -74,14 +43,6 @@ public class ProgressBarBean implements Serializable {
         this.enabled = enabled;
     }
 
-    public Long getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Long startTime) {
-        this.startTime = startTime;
-    }
-
     public boolean isButtonRendered() {
         return buttonRendered;
     }
@@ -89,4 +50,16 @@ public class ProgressBarBean implements Serializable {
     public void setButtonRendered(boolean buttonRendered) {
         this.buttonRendered = buttonRendered;
     }
+
+    public int getCurrentValue() {
+        if (!isEnabled()) {
+            return -1;
+        }
+        return currentValue;
+    }
+
+    public void setCurrentValue(int currentValue) {
+        this.currentValue = currentValue;
+    }
+
 }
