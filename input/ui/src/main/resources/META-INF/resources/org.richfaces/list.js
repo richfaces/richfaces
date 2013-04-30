512867a21f394b9de710144d695ebd276b3c6db4
Domenico <mimmozz@aruba.it>
2013-04-30 20:07:57 +0300

    RF-11691 - rich:select value disappears if you click between list and value

--- input/ui/src/main/resources/META-INF/resources/org.richfaces/list.js
+++ input/ui/src/main/resources/META-INF/resources/org.richfaces/list.js
@@ -205,6 +205,7 @@
             onClick: function(e) {
                 this.setFocus();
                 var item = this.__getItem(e);
+                if (!item) return;
                 this.processItem(item);
                 var clickModified = e.metaKey || e.ctrlKey;
                 if (!this.disabled) {
@@ -215,6 +216,7 @@
             onDblclick: function(e) {
                 this.setFocus();
                 var item = this.__getItem(e);
+                if (!item) return;
                 this.processItem(item);
                 if (!this.disabled) {
                     this.__select(item, false);
