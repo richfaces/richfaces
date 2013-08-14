b35a2b6d4299fd62e781b72faa3912064ec7f82c
Brian Leathem <bleathem@gmail.com>
2013-08-14 13:00:39 -0700

    RF-12943: Re-factored the built-in sorting and filtering integration tests into seperate test classes

--- rich/src/test/integration/org/richfaces/component/extendedDataTable/TestBuiltInFilter.java
+++ rich/src/test/integration/org/richfaces/component/extendedDataTable/TestBuiltInFilter.java
@@ -1,6 +1,7 @@
 package org.richfaces.component.extendedDataTable;
 
 import java.net.URL;
+import java.util.List;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -12,6 +13,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,7 +22,7 @@ import org.richfaces.shrinkwrap.descriptor.FaceletAsset;
 
 @RunAsClient
 @RunWith(Arquillian.class)
-public class TestBuiltInSort {
+public class TestBuiltInFilter {
 
     @Drone
     private WebDriver browser;
@@ -40,12 +42,9 @@ public class TestBuiltInSort {
     @FindBy(id = "myForm:edt:header")
     private WebElement header;
 
-    @FindBy(className = "rf-edt-srt")
-    private WebElement sortHandle;
-
     @Deployment
     public static WebArchive createDeployment() {
-        IterationDeployment deployment = new IterationDeployment(TestBuiltInSort.class);
+        IterationDeployment deployment = new IterationDeployment(TestBuiltInFilter.class);
         deployment.archive().addClass(IterationBuiltInBean.class);
         addIndexPage(deployment);
 
@@ -53,56 +52,48 @@ public class TestBuiltInSort {
     }
 
     @Test
-    public void table_sort() throws InterruptedException {
+    public void table_filter() throws InterruptedException {
         // given
         browser.get(contextPath.toExternalForm());
-        WebElement cell = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt")).get(0);
-        Assert.assertEquals("3", cell.getText());
 
-        guardXhr(sortHandle).click();
+        List<WebElement> cells = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt"));
+        Assert.assertEquals("3", cells.get(0).getText());
+        Assert.assertEquals(10, cells.size());
+
+        WebElement filterInput = browser.findElement(By.id("myForm:edt:column2:flt"));
+        filterInput.clear();
+        filterInput.sendKeys("3");
+        filterInput.sendKeys(Keys.TAB);
         Thread.sleep(500);
-        cell = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt")).get(0);
-        Assert.assertEquals("0", cell.getText());
-
-        guardXhr(sortHandle).click();
-        Thread.sleep(500);
-        cell = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt")).get(0);
-        Assert.assertEquals("9", cell.getText());
-
-        guardXhr(sortHandle).click();
-        Thread.sleep(500);
-        cell = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt")).get(0);
-        Assert.assertEquals("0", cell.getText());
-
+        cells = browser.findElements(By.cssSelector(".rf-edt-c-column2 .rf-edt-c-cnt"));
+        Assert.assertEquals("3", cells.get(0).getText());
+        Assert.assertEquals(4, cells.size());
     }
 
     private static void addIndexPage(IterationDeployment deployment) {
         FaceletAsset p = new FaceletAsset();
-        p.xmlns("rich", "http://richfaces.org/rich");
+        p.xmlns("rich", "http://richfaces.org/iteration");
         p.xmlns("a4j", "http://richfaces.org/a4j");
 
         p.body("<script type='text/javascript'>");
-        p.body("function sortEdt(currentSortOrder) { ");
-        p.body("  var edt = RichFaces.component('myForm:edt'); ");
-        p.body("  var sortOrder = currentSortOrder == 'ascending' ? 'descending' : 'ascending'; ");
-        p.body("  edt.sort('column2', sortOrder, true); ");
+        p.body("function filterEdt(filterValue) { ");
+        p.body("  var edt = RichFaces.$('myForm:edt'); ");
+        p.body("  edt.filter('column2', filterValue, true); ");
         p.body("} ");
         p.body("</script>");
         p.body("<h:form id='myForm'> ");
-        p.body("    <rich:extendedDataTable id='edt' value='#{iterationBuiltInBean.values}' var='bean' > ");
+        p.body("    <rich:extendedDataTable id='edt' value='#{iterationBuiltInBean.values}' var='bean' filterVar='fv' > ");
         p.body("        <rich:column id='column1' width='150px' > ");
         p.body("            <f:facet name='header'>Column 1</f:facet> ");
         p.body("            <h:outputText value='Bean:' /> ");
         p.body("        </rich:column> ");
         p.body("        <rich:column id='column2' width='150px' ");
-        p.body("                         sortBy='#{bean}' ");
-        p.body("                         sortOrder='#{iterationBuiltInBean.sortOrder}' > ");
+        p.body("                         filterValue='#{iterationBuiltInBean.filterValue}' ");
+        p.body("                         filterExpression='#{bean le fv}' > ");
         p.body("            <f:facet name='header'>Column 2</f:facet> ");
         p.body("            <h:outputText value='#{bean}' /> ");
         p.body("        </rich:column> ");
-        p.body("        <rich:column id='column3' width='150px'" );
-        p.body("                     sortBy='#{bean}' ");
-        p.body("                     sortOrder='#{iterationBuiltInBean.sortOrder2}' > ");
+        p.body("        <rich:column id='column3' width='150px'> ");
         p.body("            <f:facet name='header'>Column 3</f:facet> ");
         p.body("            <h:outputText value='Row #{bean}, Column 3' /> ");
         p.body("        </rich:column> ");
