306aea9e3969927bd17a6ed1ea1fe64ed446d661
Lukas Fryc <lfryc@redhat.com>
2013-09-17 23:41:03 +0200

    Moving Output module sources to API/UI modules

--- richfaces-components-ui/src/main/java/org/richfaces/component/package-info.java
+++ richfaces-components-ui/src/main/java/org/richfaces/component/package-info.java
@@ -19,6 +19,6 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-@org.richfaces.cdk.annotations.TagLibrary(uri = "http://richfaces.org/input", shortName = "input")
+@org.richfaces.cdk.annotations.TagLibrary(uri = "http://richfaces.org/output", shortName = "ou")
 package org.richfaces.component;
 
