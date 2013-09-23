7de83449e60c81fcf51df5247cab199aba4b0ee7
Lukas Fryc <lfryc@redhat.com>
2013-09-23 23:02:57 +0200

    RF-13131: upgrade Drone and Graphene, removing own version of PhantomJS binary resolution

--- build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
@@ -1,9 +1,6 @@
 package org.richfaces.arquillian.extension;
 
 import org.jboss.arquillian.core.spi.LoadableExtension;
-import org.jboss.arquillian.drone.spi.Configurator;
-import org.jboss.arquillian.drone.spi.Destructor;
-import org.jboss.arquillian.drone.spi.Instantiator;
 import org.jboss.arquillian.test.spi.enricher.resource.ResourceProvider;
 import org.richfaces.arquillian.browser.FailureScreenshot;
 import org.richfaces.arquillian.browser.PrepareBrowserSession;
@@ -11,7 +8,6 @@ import org.richfaces.arquillian.browser.ScreenshotTaker;
 import org.richfaces.arquillian.configuration.FundamentalTestConfiguratorObserver;
 import org.richfaces.arquillian.container.installation.ContainerInitializationObserver;
 import org.richfaces.arquillian.container.installation.ContainerInstaller;
-import org.richfaces.arquillian.drone.ResolvingPhantomJSDriverFactory;
 import org.richfaces.arquillian.page.source.SourceChecker;
 import org.richfaces.arquillian.page.source.SourceCheckerProvider;
 ;
@@ -28,10 +24,5 @@ public class RichFacesArquillianExtension implements LoadableExtension {
         builder.observer(PrepareBrowserSession.class);
         builder.observer(FailureScreenshot.class);
         builder.observer(ScreenshotTaker.class);
-
-        // PhantomJS with binary resolution
-        builder.service(Configurator.class, ResolvingPhantomJSDriverFactory.class);
-        builder.service(Instantiator.class, ResolvingPhantomJSDriverFactory.class);
-        builder.service(Destructor.class, ResolvingPhantomJSDriverFactory.class);
     }
 }
