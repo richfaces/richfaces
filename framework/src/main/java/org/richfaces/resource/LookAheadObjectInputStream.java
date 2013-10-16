0f3302c614a5cff55faa8f5faccba5335a26cf59
Brian Leathem <bleathem@gmail.com>
2013-10-16 12:31:53 -0700

    RF-13268: Corrected typo on the error message of the catch clause on stream.close in the loadWhitelist() method.

--- framework/src/main/java/org/richfaces/resource/LookAheadObjectInputStream.java
+++ framework/src/main/java/org/richfaces/resource/LookAheadObjectInputStream.java
@@ -132,7 +132,7 @@ public class LookAheadObjectInputStream extends ObjectInputStream {
                 try {
                     stream.close();
                 } catch (IOException e) {
-                    throw new RuntimeException("Error closing the ResourceBuilder.properties file", e);
+                    throw new RuntimeException("Error closing the resource-serialization.properties file", e);
                 }
             }
         }
