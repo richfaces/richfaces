ad6d36009efd571faba74918fab9e0e69797618a
Juraj Huska <jhuska@redhat.com>
2013-10-31 12:36:55 +0100

    RF-13303: page-fragments tests changed to be mocked
    * they were Arquillian Graphene tests, however, they are just unit tests, they
      can be just mocked

--- build/page-fragments/src/test/java/org/richfaces/tests/page/fragments/test/choicePicker/TestChoicePickerByVisibleText.java
+++ build/page-fragments/src/test/java/org/richfaces/tests/page/fragments/test/choicePicker/TestChoicePickerByVisibleText.java
@@ -1,36 +1,21 @@
 package org.richfaces.tests.page.fragments.test.choicePicker;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.jboss.arquillian.drone.api.annotation.Drone;
-import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.richfaces.tests.page.fragments.impl.utils.picker.ChoicePickerHelper;
 import org.richfaces.tests.page.fragments.impl.utils.picker.ChoicePickerHelper.ByVisibleTextChoicePicker;
-import org.richfaces.tests.page.fragments.test.choicePicker.fragments.MyPageFragment;
 
-@RunWith(Arquillian.class)
-public class TestChoicePickerByVisibleText {
-
-    @Drone
-    private WebDriver browser;
-
-    @FindBy(tagName = "body")
-    private MyPageFragment myFragment;
+public class TestChoicePickerByVisibleText extends AbstractChoicePickerTest {
 
     @Test
     public void testMultipleChoicePickerNonExistingElement() {
-        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
-
         ByVisibleTextChoicePicker picker = ChoicePickerHelper.byVisibleText().match("non existing");
         List<WebElement> elements = picker.pickMultiple(myFragment.getDivs());
         assertTrue(elements.isEmpty());
@@ -38,8 +23,6 @@ public class TestChoicePickerByVisibleText {
 
     @Test
     public void testPickExistingElement() {
-        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
-
         ByVisibleTextChoicePicker picker = ChoicePickerHelper.byVisibleText().match("1");
         WebElement element = picker.pick(myFragment.getDivs());
         assertNotNull(element);
@@ -48,8 +31,6 @@ public class TestChoicePickerByVisibleText {
 
     @Test
     public void testPickNotExistingElement() {
-        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
-
         ByVisibleTextChoicePicker picker = ChoicePickerHelper.byVisibleText().match("non existing");
         WebElement element = picker.pick(myFragment.getDivs());
         assertNull(element);
