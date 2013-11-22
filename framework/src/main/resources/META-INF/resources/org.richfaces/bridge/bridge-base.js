3df442e64957254e05b9745e4d16f44636d678a5
Lukas Fryc <lfryc@redhat.com>
2013-11-22 19:13:45 +0100

    RF-13375: bridge-base: removed logic around non-implemented cleanDom.RICH event

--- framework/src/main/resources/META-INF/resources/org.richfaces/bridge/bridge-base.js
+++ framework/src/main/resources/META-INF/resources/org.richfaces/bridge/bridge-base.js
@@ -2,33 +2,8 @@
 
   $.widget('rf.bridgeBase', {
 
-    options : {
-      pluginNames : null
-    },
-
     _create : function() {
       this.element.data('rf.bridge', this);
-      this._registerCleanDomListener(this.element, this.options.pluginNames);
-    },
-
-    _destroy : function() {
-      this._unRegisterCleanDomListener();
-    },
-
-    _registerCleanDomListener : function(element, pluginNames) {
-      $('body').on("cleanDom.RICH", function(event, ui) {
-        if ($.contains(ui.target, element)) {
-          $.each(pluginNames, function() {
-            var pluginName = this;
-            $(element).data(pluginName).destroy();
-          })
-
-        }
-      });
-    },
-
-    _unRegisterCleanDomListener : function() {
-      $('body').off("cleanDom.RICH");
     },
 
     _addUnitsIfRequired: function(options, property) {
