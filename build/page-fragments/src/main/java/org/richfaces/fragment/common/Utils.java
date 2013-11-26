49a29843c1dbecdc3ef1d0fd999369074560addd
Pavol Pitonak <ppitonak@redhat.com>
2013-11-26 14:37:19 +0100

    method invokeRichFacesJSAPIFunction in page fragments utils refactored in order to prevent ClassCastException

--- build/page-fragments/src/main/java/org/richfaces/fragment/common/Utils.java
+++ build/page-fragments/src/main/java/org/richfaces/fragment/common/Utils.java
@@ -113,7 +113,7 @@ public final class Utils {
      * @param functionWithParams JS API function with params (e.g. <code>setValue(new Date('October 10, 2012 12:00:00'))</code>  )
      */
     public static String invokeRichFacesJSAPIFunction(WebElement componentRoot, String functionWithParams) {
-        return (String) getExecutorFromElement(componentRoot).executeScript("return RichFaces.component(arguments[0])." + functionWithParams, componentRoot);
+        return getExecutorFromElement(componentRoot).executeScript("return RichFaces.component(arguments[0])." + functionWithParams, componentRoot).toString();
     }
 
     public static boolean isVisible(WebElement e) {
