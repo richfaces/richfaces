32067d651fa0bf76126a99f09734e2b06a9889d1
Lukas Fryc <lfryc@redhat.com>
2014-01-02 10:47:23 +0100

    RF-13432: fix InplaceInputRendererTest by fixing typo that fails to load XHTML resource on Mojarra 2.2

--- framework/src/test/java/org/richfaces/ui/input/InplaceInputRendererTest.java
+++ framework/src/test/java/org/richfaces/ui/input/InplaceInputRendererTest.java
@@ -65,7 +65,7 @@ public class InplaceInputRendererTest extends InplaceRendererTestBase {
 
     @Test
     public void testEditWithControls() throws Exception {
-        HtmlPage page = environment.getPage("//inplaceInputTest.jsf");
+        HtmlPage page = environment.getPage("/inplaceInputTest.jsf");
         String withControlsComponentId = BASE_ID + WITH_CONTROLS;
         edit(page, withControlsComponentId, "Another Test String");
 
