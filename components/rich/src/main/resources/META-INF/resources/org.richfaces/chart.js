51dba0a4afa3014915308208586ba686cc265f98
Lukas Macko <llukas3@gmail.com>
2014-05-06 20:04:41 +0200

    pie chart

--- components/rich/src/main/resources/META-INF/resources/org.richfaces/chart.js
+++ components/rich/src/main/resources/META-INF/resources/org.richfaces/chart.js
@@ -21,6 +21,168 @@
  */
 
 (function ($, rf) {
+	var defaultOptions = {
+
+	      /**
+	       * Specifies the type of chart.
+	       * According to this property and xtypex ytype, proper default options corresponding to chart type
+	       * are applied and data are transformed to the format expected by flot.
+	       * Following values are supported: `line`,`bar`,`pie`
+	       * Options is not required. If it is not used the options and data format remains the same as flot uses by default.
+	       * @property charttype
+	       * @default null
+	       */
+	      charttype: '',
+
+	      /**
+	       * Specify the data type of values plotted on x-axis
+	       * Following options are supported: number,string,date
+	       * @property xtype
+	       * @default null
+	       */
+	      xtype: '',
+
+	      /**
+	       * Specify the data type of values plotted on x-axis
+	       * Following options are supported: number,string,date
+	       * @property xtype
+	       * @default null
+	       */
+	      ytype: '',
+
+	      /**
+	       * Allows to zoom the chart. Supported only when line chart is used. Requires charttype to be set.
+	       * @property zoom
+	       * @default false
+	       */
+	      zoom: false,
+
+
+
+	     /**
+	      * Options customizing the chart grid.
+	      * @type Object
+	      * @default {clickable:true, hoverable:true}
+	      * @property grid
+	      *   @property clickable {boolean}
+	      *   Grid accepts click events.
+	      *   @property hoverable {boolean}
+	      *   Grid fires mouseover event. Necessary for tooltip to work.
+	      *
+	      */
+	     grid: {
+	        clickable: true,
+	        hoverable: true
+	      },
+
+	      /**
+	       * Turns on tooltip (text shown when mouse points to a part of a chart)
+	       * @property tooltip
+	       * @type boolean
+	       * @default true
+	       */
+	      tooltip: true,
+
+	      /**
+	       * Customizes the tooltip.
+	       * @type Object
+	       * @default { content: '%s [%x,%y]'}
+	       * @property tooltipOpts
+	       *    @property content {String}
+	       *    Specify the tooltip format. Use %s for series label, %x for X values, %y for Y value
+	       *    @property defaultTheme
+	       */
+	      tooltipOpts: {
+	        content: '%s  [%x,%y]',
+	        shifts: {
+	          x: 20,
+	          y: 0
+	        },
+	        defaultTheme: false
+	      },
+
+	      /**
+	       * Legend properties
+	       * @type Object
+	       * @default {postion:'ne', sorted: 'ascending'}
+	       * @property legend
+	       *    @property position {String}
+	       *    Defines the placement of the legend in the grid. One of ne,nw,se,sw
+	       *    @property sorted {String}
+	       *    Defines the order of labels in the legend. One of ascending,descending,false.
+	       */
+	      legend: {
+	        postion:'ne',
+	        sorted: 'ascending'
+	      },
+
+	      /**
+	       * Customizes the horizontal axis
+	       * @type Object
+	       * @default {min: null, max: null,autoscaleMargin: null, axisLabel: ''}
+	       * @property xaxis
+	       *   @property min {Number}
+	       *   Minimal value shown on axis
+	       *   @property max {Number}
+	       *   Maximal values show on axis
+	       *   @property autoscaleMargin {Number}
+	       *   It's the fraction of margin that the scaling algorithm will add
+	       *   to avoid that the outermost points ends up on the grid border
+	       *   @property axisLabel {String}
+	       *   Axis description
+	       */
+	      xaxis:{
+	        min: null,
+	        max: null,
+	        autoscaleMargin: null,
+	        axisLabel: ''
+	      },
+
+	      /**
+	       * Customizes the vertical axis
+	       * @type Object
+	       * @default {min: null, max: null,autoscaleMargin: 0.2,axisLabel: ''}
+	       * @property yaxis
+	       *   @property min {Number}
+	       *   Minimal value shown on axis
+	       *   @property max {Number}
+	       *   Maximal values show on axis
+	       *   @property autoscaleMargin {Number}
+	       *   It's the fraction of margin that the scaling algorithm will add to
+	       *   avoid that the outermost points ends up on the grid border.
+	       *   @property axisLabel {String}
+	       *   Axis description
+	       */
+	      yaxis:{
+	        min: null,
+	        max: null,
+	        autoscaleMargin: 0.2,
+	        axisLabel: ''
+	      },
+
+
+
+
+	      /**
+	       * Data to be plotted. The format is the same used by flot. The format may differ if the charttype
+	       * option is set to pie or bar.
+	       * @property data
+	       * @default []
+	       */
+	      data:[]
+
+	    };
+	    
+	var pieDefaults = {
+	      series: {
+	          pie: {
+	            show: true
+	          }
+	        },
+	        tooltipOpts: {
+	          content: ' %p.0%, %s'
+	        }
+	      };
 
     rf.rf4 = rf.rf4 || {};
     rf.rf4.ui = rf.rf4.ui || {};
@@ -30,8 +192,19 @@
             name:"Chart",
 
             init : function (componentId, options) {
-            	$super.constructor.call(this, componentId, options);
+            	options = $.extend(true,defaultOptions,options);
+            	
             	this.element = $(document.getElementById(componentId));
+            	
+            	if (options.charttype === 'pie') {
+                    options = $.extend(true,options, pieDefaults);
+                    //transform data from
+                    // [[{data:1, label:"label1"},{data:2,label:"label2"},...]]
+                    //to
+                    // [{data:1, label:"label1"},{data:2,label:"label2"},...]
+                    options.data = options.data[0]; //pie chart data should not be in a collection
+                }
+            	$super.constructor.call(this, componentId, options);
                 $.plot(this.element,options.data,options);
             },
 
