777d8c562ad84eb6150359ce18d9424d0bbd115f
Brian Leathem <bleathem@gmail.com>
2014-06-13 14:11:02 -0700

    RF-13636: added java.util.Collection to whitelistBaseClasses

--- core/src/main/java/org/richfaces/util/LookAheadObjectInputStream.java
+++ core/src/main/java/org/richfaces/util/LookAheadObjectInputStream.java
@@ -26,6 +26,7 @@ import java.io.InputStream;
 import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -76,6 +77,7 @@ public class LookAheadObjectInputStream extends ObjectInputStream {
         whitelistBaseClasses.add(Byte.class);
         whitelistBaseClasses.add(Character.class);
         whitelistBaseClasses.add(Number.class);
+        whitelistBaseClasses.add(Collection.class);
 
         loadWhitelist();
     }
