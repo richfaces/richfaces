4aab2f391579d752d67b5c39bdc20d4fc6ebe506
Juraj Huska <jhuska@redhat.com>
2014-08-14 15:52:44 +0200

    RF-13777: Custom RF screenshooter deleted
    -screenshoting mechanism is now provided by Graphene itself
    -profile take-screenshots added, it just needs to he on the classpath in order to take screenshots
    -default setting is that it takes screenshots only when test fails, can be changed in arquillian.xml

--- build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
@@ -2,9 +2,7 @@ package org.richfaces.arquillian.extension;
 
 import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.arquillian.test.spi.enricher.resource.ResourceProvider;
-import org.richfaces.arquillian.browser.FailureScreenshot;
 import org.richfaces.arquillian.browser.PrepareBrowserSession;
-import org.richfaces.arquillian.browser.ScreenshotTaker;
 import org.richfaces.arquillian.configuration.FundamentalTestConfiguratorObserver;
 import org.richfaces.arquillian.container.installation.ContainerInitializationObserver;
 import org.richfaces.arquillian.container.installation.ContainerInstaller;
@@ -23,8 +21,6 @@ public class RichFacesArquillianExtension implements LoadableExtension {
         builder.observer(ContainerInitializationObserver.class);
         builder.observer(ContainerInstaller.class);
         builder.observer(PrepareBrowserSession.class);
-        builder.observer(FailureScreenshot.class);
-        builder.observer(ScreenshotTaker.class);
         builder.observer(VerifyDeploymentTestability.class);
     }
 }
