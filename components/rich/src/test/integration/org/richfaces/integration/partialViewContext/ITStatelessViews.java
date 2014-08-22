6d102c9b5a31c1427713f245b7825a2889c661c0
Michal Petrov <mpetrov@redhat.com>
2014-08-22 14:02:18 +0200

    changing version to 4.5.0.Beta1

--- components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
+++ components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
@@ -47,7 +47,7 @@ public class ITStatelessViews {
     public static WebArchive createDeployment() {
         RichDeployment deployment = new RichDeployment(ITStatelessViews.class);
 
-        deployment.addMavenDependency("org.richfaces:richfaces-a4j:4.5.0-SNAPSHOT");
+        deployment.addMavenDependency("org.richfaces:richfaces-a4j:4.5.0.Beta1");
 
         addIndexPage(deployment);
 
