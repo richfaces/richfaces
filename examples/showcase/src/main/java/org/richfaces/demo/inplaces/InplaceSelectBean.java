18211df05388f7243980b4397fa4e3ec022b5bb9
Brian Leathem <bleathem@gmail.com>
2014-08-29 09:00:19 -0700

    RF-13804: Added an autcomplete sample to the select component

--- examples/showcase/src/main/java/org/richfaces/demo/inplaces/InplaceSelectBean.java
+++ examples/showcase/src/main/java/org/richfaces/demo/inplaces/InplaceSelectBean.java
@@ -1,14 +1,20 @@
 package org.richfaces.demo.inplaces;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import org.richfaces.demo.tables.model.capitals.Capital;
 
 @ManagedBean
@@ -25,6 +31,19 @@ public class InplaceSelectBean {
         for (Capital capital : capitals) {
             capitalsOptions.add(new SelectItem(capital.getName(), capital.getState()));
         }
+    }
+
+    public Collection<Capital> autocomplete(FacesContext facesContext, UIComponent component, final String prefix) {
+        Collection<Capital> persons = Collections2.filter(capitals, new Predicate<Capital>() {
+            @Override
+            public boolean apply(Capital capital) {
+                if (prefix == null) {
+                    return true;
+                }
+                return capital.getState().toLowerCase().startsWith(prefix.toLowerCase());
+            }
+        });
+        return persons;
     }
 
     public List<SelectItem> getCapitalsOptions() {
