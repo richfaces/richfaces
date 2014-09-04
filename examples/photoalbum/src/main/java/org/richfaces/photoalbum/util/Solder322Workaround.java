ade94c9c7147e9775ea73aff70f422e184482b02
Michal Petrov <mpetrov@redhat.com>
2014-09-04 15:39:20 +0200

    RF-13814: fix deployment on EAP, fix JIRA reference

--- examples/photoalbum/src/main/java/org/richfaces/photoalbum/util/Solder322Workaround.java
+++ examples/photoalbum/src/main/java/org/richfaces/photoalbum/util/Solder322Workaround.java
@@ -14,8 +14,8 @@ import javax.servlet.http.HttpSession;
 import org.jboss.solder.reflection.annotated.AnnotatedTypeBuilder;
 import org.jboss.solder.servlet.http.ImplicitHttpServletObjectsProducer;
 
-// workaround for SOLDER-332
-public class Solder332Workaround implements Extension {
+// workaround for SOLDER-322
+public class Solder322Workaround implements Extension {
 
     public void vetoHttpServletObjectsProducer(@Observes ProcessAnnotatedType<ImplicitHttpServletObjectsProducer> event) {
         AnnotatedTypeBuilder<ImplicitHttpServletObjectsProducer> builder = new AnnotatedTypeBuilder<ImplicitHttpServletObjectsProducer>();
@@ -24,6 +24,12 @@ public class Solder332Workaround implements Extension {
         // remove producer methods for HttpServletRequest, HttpSession and ServletContext
         for (AnnotatedMethod<? super ImplicitHttpServletObjectsProducer> method : event.getAnnotatedType().getMethods()) {
             Type type = method.getBaseType();
+            
+            // skip when Photoalbum isn't deployed on WildFly
+            // EAP methods' class is "class org.jboss.weld.introspector.jlr.WeldMethodImpl"
+            if (!method.getClass().toString().equals("class org.jboss.weld.annotated.slim.backed.BackedAnnotatedMethod")) {
+                continue;
+            }
             if (HttpServletRequest.class.equals(type) || HttpSession.class.equals(type) || ServletContext.class.equals(type)) {
                 builder.removeFromMethod(method, Produces.class);
             }
