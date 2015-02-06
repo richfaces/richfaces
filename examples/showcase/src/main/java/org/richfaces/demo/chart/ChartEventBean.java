1b7c3f1a20376bd8a2eceabb5ce6e69a5f5036a7
Michal Petrov <mpetrov@redhat.com>
2015-02-06 12:58:07 +0100

    change event handling (RF-13939)

--- examples/showcase/src/main/java/org/richfaces/demo/chart/ChartEventBean.java
+++ examples/showcase/src/main/java/org/richfaces/demo/chart/ChartEventBean.java
@@ -32,51 +32,43 @@ import javax.faces.bean.ViewScoped;
 
 import org.richfaces.model.PlotClickEvent;
 
-
-
-
 @ManagedBean
 @ViewScoped
-public class ChartEventBean implements Serializable{
-
+public class ChartEventBean implements Serializable {
 
     private static final long serialVersionUID = 506812532467786866L;
     List<Country> countries;
     String msg;
 
-
-
     @PostConstruct
-    public void init(){
-        msg="no server-side event";
-
+    public void init() {
+        msg = "no server-side event";
 
         Country usa = new Country("USA");
-        usa.put(1990,19.1);
-        usa.put(1991,18.9);
-        usa.put(1992,18.6);
-        usa.put(1993,19.5);
-        usa.put(1994,19.5);
-        usa.put(1995,19.3);
-        usa.put(1996,19.4);
-        usa.put(1997,19.7);
-        usa.put(1998,19.5);
-        usa.put(1999,19.5);
-        usa.put(2000,20.0);
+        usa.put(1990, 19.1);
+        usa.put(1991, 18.9);
+        usa.put(1992, 18.6);
+        usa.put(1993, 19.5);
+        usa.put(1994, 19.5);
+        usa.put(1995, 19.3);
+        usa.put(1996, 19.4);
+        usa.put(1997, 19.7);
+        usa.put(1998, 19.5);
+        usa.put(1999, 19.5);
+        usa.put(2000, 20.0);
 
         Country china = new Country("China");
-        china.put(1990,2.2);
-        china.put(1991,2.2);
-        china.put(1992,2.3);
-        china.put(1993,2.4);
-        china.put(1994,2.6);
-        china.put(1995,2.7);
-        china.put(1996,2.8);
-        china.put(1997,2.8);
-        china.put(1998,2.7);
-        china.put(1999,2.6);
-        china.put(2000,2.7);
-
+        china.put(1990, 2.2);
+        china.put(1991, 2.2);
+        china.put(1992, 2.3);
+        china.put(1993, 2.4);
+        china.put(1994, 2.6);
+        china.put(1995, 2.7);
+        china.put(1996, 2.8);
+        china.put(1997, 2.8);
+        china.put(1998, 2.7);
+        china.put(1999, 2.6);
+        china.put(2000, 2.7);
 
         Country japan = new Country("Japan");
         japan.put(1990, 9.4);
@@ -92,17 +84,15 @@ public class ChartEventBean implements Serializable{
         japan.put(2000, 9.7);
 
         Country russia = new Country("Russia");
-        russia.put(1992,14);
-        russia.put(1993,12.8);
-        russia.put(1994,10.9);
-        russia.put(1995,10.5);
-        russia.put(1996,10.4);
-        russia.put(1997,10);
-        russia.put(1998,9.6);
-        russia.put(1999,9.7);
-        russia.put(2000,9.8);
-
-
+        russia.put(1992, 14);
+        russia.put(1993, 12.8);
+        russia.put(1994, 10.9);
+        russia.put(1995, 10.5);
+        russia.put(1996, 10.4);
+        russia.put(1997, 10);
+        russia.put(1998, 9.6);
+        russia.put(1999, 9.7);
+        russia.put(2000, 9.8);
 
         countries = new LinkedList<Country>();
         countries.add(usa);
@@ -110,11 +100,7 @@ public class ChartEventBean implements Serializable{
         countries.add(japan);
         countries.add(russia);
 
-
     }
-
-
-
 
     public String getMsg() {
         return msg;
@@ -124,20 +110,23 @@ public class ChartEventBean implements Serializable{
         return countries;
     }
 
-    public void handler(PlotClickEvent event){
-        msg = event.toString();
+    public void handler(PlotClickEvent event) {
+        if (event.getSeriesIndex() >= 0) {
+            msg = event.toString();
+        }
     }
 
-    public class Country{
+    public class Country {
         private final String name;
-        //CO2 production year-> metric tons per capita
+        // CO2 production year-> metric tons per capita
         private final List<Record> data;
 
-        public Country(String name){
+        public Country(String name) {
             this.name = name;
             this.data = new LinkedList<Record>();
         }
-        public void put(int year, double tons){
+
+        public void put(int year, double tons) {
             this.data.add(new Record(year, tons));
         }
 
@@ -149,7 +138,7 @@ public class ChartEventBean implements Serializable{
             return name;
         }
 
-        public class Record{
+        public class Record {
             private final int year;
             private final double tons;
 
