1153a9685b31b74fa8731482a4fb6221fe06a8f8
Michal Petrov <mpetrov@redhat.com>
2015-05-19 12:40:52 +0200

    RF-14044: force pushresource mime-type

--- core/src/main/resources/META-INF/resources/net.java.dev.atmosphere/jquery-atmosphere.js
+++ core/src/main/resources/META-INF/resources/net.java.dev.atmosphere/jquery-atmosphere.js
@@ -2787,14 +2787,16 @@
                             async: _request.closeAsync,
                             timeout: _request.connectTimeout,
                             cache: false,
-                            crossDomain: _request.enableXDR
+                            crossDomain: _request.enableXDR,
+                            mimeType: "text/plain"
                         });
                     } else {
                         jQuery.ajax({
                             url: url,
                             async: _request.closeAsync,
                             cache: false,
-                            crossDomain: _request.enableXDR
+                            crossDomain: _request.enableXDR,
+                            mimeType: "text/plain"
                         });
                     }
                 }
