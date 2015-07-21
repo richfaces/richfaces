580c0bf8a73f01ee75a457940d33476fbb733bce
Matej Novotny <manovotn@redhat.com>
2015-07-21 13:57:16 +0200

    photoalbum: fixed test for G+ login; the UI for logging was changed and caused tests to fail

--- examples/photoalbum/src/test/java/org/richfaces/photoalbum/ftest/webdriver/pages/GPlusLoginPage.java
+++ examples/photoalbum/src/test/java/org/richfaces/photoalbum/ftest/webdriver/pages/GPlusLoginPage.java
@@ -1,24 +1,18 @@
-/*******************************************************************************
- * JBoss, Home of Professional Open Source
- * Copyright 2010-2014, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+/**
+ * *****************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2014, Red Hat, Inc. and individual contributors by the @authors tag.
+ * See the copyright.txt in the distribution for a full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *******************************************************************************/
+ * You should have received a copy of the GNU Lesser General Public License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * *****************************************************************************
+ */
 package org.richfaces.photoalbum.ftest.webdriver.pages;
 
 import org.jboss.arquillian.graphene.Graphene;
@@ -36,6 +30,8 @@ public class GPlusLoginPage implements SocialLoginPage {
     private TextInputComponentImpl emailInput;
     @FindBy(css = "input[id$=Passwd]")
     private TextInputComponentImpl passwordInput;
+    @FindBy(css = "input[id$=next]")
+    private WebElement nextButton;
     @FindBy(css = "input[id$=signIn]")
     private WebElement loginButton;
 
@@ -43,6 +39,8 @@ public class GPlusLoginPage implements SocialLoginPage {
     public void login(String email, String password) {
         Graphene.waitModel().until().element(emailInput.advanced().getInputElement()).is().visible();
         emailInput.advanced().clear(ClearType.DELETE).sendKeys(email);
+        nextButton.click();
+        Graphene.waitAjax().until().element(passwordInput.advanced().getInputElement()).is().visible();
         passwordInput.advanced().clear(ClearType.DELETE).sendKeys(password);
         loginButton.click();
     }
