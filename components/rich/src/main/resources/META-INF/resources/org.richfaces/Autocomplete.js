f6b3f8a026e2dd908a74d5f1352d2dd2fe026fe7
Michal Petrov <mpetrov@redhat.com>
2015-12-01 13:57:16 +0100

    RF-11902: add queueId handling for autocomplete

--- components/rich/src/main/resources/META-INF/resources/org.richfaces/Autocomplete.js
+++ components/rich/src/main/resources/META-INF/resources/org.richfaces/Autocomplete.js
@@ -175,7 +175,7 @@
         //caution: JSF submits inputs with empty names causing "WARNING: Parameters: Invalid chunk ignored." in Tomcat log
         var params = {};
         params[this.id + ".ajax"] = "1";
-        rf.ajax(this.id, event, {parameters: params, error: ajaxError, complete:ajaxSuccess});
+        rf.ajax(this.id, event, {parameters: params, error: ajaxError, complete:ajaxSuccess, queueId: _this.options.queueId});
     };
 
     var clearSelection = function () {
