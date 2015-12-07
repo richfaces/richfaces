ec5e3e3327d684a825d3e8b7f3c0d3f98edaef86
Michal Petrov <mpetrov@redhat.com>
2015-12-07 17:58:39 +0100

    RF-14207: specify execute default for a4j:ajax

--- components/a4j/src/main/java/org/ajax4jsf/component/behavior/AjaxBehavior.java
+++ components/a4j/src/main/java/org/ajax4jsf/component/behavior/AjaxBehavior.java
@@ -192,7 +192,8 @@ public class AjaxBehavior extends ClientBehavior implements AjaxClientBehavior, 
      * Ids of components that will participate in the "execute" portion of the Request Processing Lifecycle.
      * Can be a single id, a space or comma separated list of Id's, or an EL Expression evaluating to an array or Collection.
      * Any of the keywords "@this", "@form", "@all", "@none", "@region" may be specified in the identifier list.
-     * Some components make use of additional keywords
+     * Some components make use of additional keywords.<br/>
+     * Default value is "@region" which resolves to parent component if no region is present.
      */
     @Attribute
     public Object getExecute() {
