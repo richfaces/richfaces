225aabec5b1a67a2943183b993f6df7c800bc8a8
Michal Petrov <mpetrov@redhat.com>
2015-12-11 14:17:19 +0100

    changing version to 4.5.12.Final

--- build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/configuration/FundamentalTestConfiguration.java
@@ -10,7 +10,7 @@ public class FundamentalTestConfiguration implements DroneConfiguration<Fundamen
 
     private String richfacesVersion;
     private Boolean servletContainerSetup;
-    private String currentBuildRichfacesVersion = "4.5.12-SNAPSHOT";
+    private String currentBuildRichfacesVersion = "4.5.12.Final";
     private String jsfProvider;
     private String jsfImplementation;
     private String container;
