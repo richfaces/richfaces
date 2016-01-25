7a6bd665fef08cdddb79f555702bdd471c138aac
Jiri Stefek <jstefek@redhat.com>
2016-01-25 09:37:28 +0100

    showcase: tests: autocomplete: added waiting for suggestions to be visible in ITestCustomLayouts (RF-14223)

--- examples/showcase/src/test/java/org/richfaces/showcase/autocomplete/ITestCustomLayouts.java
+++ examples/showcase/src/test/java/org/richfaces/showcase/autocomplete/ITestCustomLayouts.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * JBoss, Home of Professional Open Source
  * Copyright 2010-2014, Red Hat, Inc. and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
@@ -18,11 +18,11 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *******************************************************************************/
+ */
 package org.richfaces.showcase.autocomplete;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.jboss.arquillian.graphene.page.Page;
 import org.junit.Test;
@@ -34,64 +34,47 @@ import org.richfaces.showcase.autocomplete.page.CustomLayoutsPage;
 
 /**
  * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
- * @version $Revision$
  */
 public class ITestCustomLayouts extends AbstractWebDriverTest {
 
     @Page
     public CustomLayoutsPage page;
 
-    /* ********************************************************************************************************************
-     * Constants *********************************************************************************************************
-     * ***********
-     */
-
     private final String EXP_SUGG_AFTER_V_FST_INPUT = "Vermont Montpelier";
     private final String EXP_SUGG_AFTER_ALA_FST_INPUT = "Alabama Montgomery";
     private final String EXP_SUGG_AFTER_V_SC_INPUT = "Vermont - (Montpelier)";
     private final String EXP_SUGG_AFTER_ALA_SC_INPUT = "Alabama - (Montgomery)";
 
-    /* **********************************************************************************************************************
-     * Tests *************************************************************************************************************
-     * ********
-     */
-
     @Test
     public void testAutocompletionOfInputWithTableLayout() {
-
-        typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(page.getAutocomplete1(), "v",
+        typeSomethingToInput_checkThePopup_selectFirst_checkTheInputValue(page.getAutocomplete1(), "v",
             EXP_SUGG_AFTER_V_FST_INPUT, EXP_SUGG_AFTER_V_FST_INPUT.split(" ")[0]);
 
-        typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(page.getAutocomplete1(), "ala",
+        typeSomethingToInput_checkThePopup_selectFirst_checkTheInputValue(page.getAutocomplete1(), "ala",
             EXP_SUGG_AFTER_ALA_FST_INPUT, EXP_SUGG_AFTER_ALA_FST_INPUT.split(" ")[0]);
     }
 
     @Test
-    public void testAutocompletionOfInputWithDivlayput() {
-
-        typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(page.getAutocomplete2(), "v", EXP_SUGG_AFTER_V_SC_INPUT,
+    public void testAutocompletionOfInputWithDivLayput() {
+        typeSomethingToInput_checkThePopup_selectFirst_checkTheInputValue(page.getAutocomplete2(), "v", EXP_SUGG_AFTER_V_SC_INPUT,
             EXP_SUGG_AFTER_V_SC_INPUT.split(" ")[0]);
 
-        typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(page.getAutocomplete2(), "ala",
+        typeSomethingToInput_checkThePopup_selectFirst_checkTheInputValue(page.getAutocomplete2(), "ala",
             EXP_SUGG_AFTER_ALA_SC_INPUT, EXP_SUGG_AFTER_ALA_SC_INPUT.split(" ")[0]);
 
     }
 
-    /* ******************************************************************************************************************
-     * Help methods ******************************************************************************************************
-     * ************
-     */
-
-    private void typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(RichFacesAutocomplete autocomplete,
+    private void typeSomethingToInput_checkThePopup_selectFirst_checkTheInputValue(RichFacesAutocomplete autocomplete,
         String whatTotype, String expectedValueInPopup, String expectedValueInInputAfterEnter) {
 
         SelectOrConfirm suggestions = autocomplete.type(whatTotype);
 
+        autocomplete.advanced().waitForSuggestionsToBeVisible().perform();
         String firstSuggestion = autocomplete.advanced().getSuggestionsElements().get(0).getText();
-        assertTrue("The first row of popup should suggest " + expectedValueInPopup + " " + "when " + whatTotype
+        assertTrue("The first row of popup should suggest " + expectedValueInPopup + " when " + whatTotype
             + " is typed in input", expectedValueInPopup.equals(firstSuggestion));
 
-        suggestions.select(ChoicePickerHelper.byVisibleText().contains(firstSuggestion));
+        suggestions.select(ChoicePickerHelper.byIndex().first());
 
         String valueInInput = autocomplete.advanced().getInput().getStringValue();
         assertEquals("The value in input should be different!", expectedValueInInputAfterEnter, valueInInput);
