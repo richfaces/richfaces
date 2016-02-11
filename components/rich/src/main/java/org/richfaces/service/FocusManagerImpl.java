512a68c5ff26d1301c3941db740ef4d2c56d39e5
Michal Petrov <mpetrov@redhat.com>
2016-02-11 16:39:38 +0100

    RF-14231: make focusManager work inside iteration components

--- components/rich/src/main/java/org/richfaces/service/FocusManagerImpl.java
+++ components/rich/src/main/java/org/richfaces/service/FocusManagerImpl.java
@@ -1,5 +1,8 @@
 package org.richfaces.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -11,6 +14,7 @@ import org.richfaces.javascript.JavaScriptService;
 import org.richfaces.log.Logger;
 import org.richfaces.log.RichfacesLogger;
 import org.richfaces.renderkit.util.RendererUtils;
+import org.richfaces.util.SeparatorChar;
 
 public class FocusManagerImpl implements FocusManager {
 
@@ -18,6 +22,8 @@ public class FocusManagerImpl implements FocusManager {
     private static final String SCRIPT = "RichFaces.jQuery(document.getElementById('%s'))"
                                        + ".find(':text:visible:first').addBack().focus();";
     private static final Logger LOG = RichfacesLogger.APPLICATION.getLogger();
+    private static final String ROW_ID = SeparatorChar.SEPARATOR_CHAR + "\\d+" + SeparatorChar.SEPARATOR_CHAR;
+    private static final String ROW_PATTERN = "(" + ROW_ID + ")(.+)";
 
     @Override
     public void focus(String componentId) {
@@ -35,15 +41,37 @@ public class FocusManagerImpl implements FocusManager {
             if (currentComponent == null) {
                 currentComponent = context.getViewRoot();
             }
-            currentComponent.findComponent(componentId);
-            UIComponent component = RendererUtils.getInstance().findComponentFor(currentComponent, componentId);
+            Pattern rowPattern = Pattern.compile(ROW_PATTERN);
+            Matcher rowMatcher = rowPattern.matcher(componentId);
+
+            String newId = componentId;
+
+            if (rowMatcher.matches()) {
+                // componentId is ":<number>:id", target is inside iteration component
+                // findComponent() doesn't expand iteration components, we let it find the base one
+                newId = rowMatcher.group(2);
+                // replace any additional row indices
+                newId = newId.replaceAll(ROW_ID, String.valueOf(SeparatorChar.SEPARATOR_CHAR));
+            }
+
+            UIComponent component = RendererUtils.getInstance().findComponentFor(currentComponent, newId);
 
             if (component == null) {
-                String message = FocusManager.class.getSimpleName() + ": Component with ID '" + componentId + "' was not found";
-                LOG.warn(message);
-                context.addMessage(null, new FacesMessage(message));
+                logNotFound(context, componentId);
             } else {
                 String clientId = component.getClientId(context);
+
+                if (rowMatcher.matches()) {
+                    int end = clientId.lastIndexOf(SeparatorChar.SEPARATOR_CHAR + newId);
+                    if (end != -1) {
+                        // insert row number into the id
+                        clientId = clientId.substring(0, end) + componentId;
+                    } else {
+                        // the parent form has prependId="false" and the input was found outside any iteration component
+                        logNotFound(context, componentId);
+                        return;
+                    }
+                }
 
                 setContextAttribute(context, clientId);
 
@@ -55,6 +83,12 @@ public class FocusManagerImpl implements FocusManager {
 
     private void setContextAttribute(FacesContext context, String clientId) {
         context.getAttributes().put(FocusManager.FOCUS_CONTEXT_ATTRIBUTE, clientId);
+    }
+
+    private void logNotFound(FacesContext context, String id) {
+        String message = FocusManager.class.getSimpleName() + ": Component with ID '" + id + "' was not found";
+        LOG.warn(message);
+        context.addMessage(null, new FacesMessage(message));
     }
 }
 
