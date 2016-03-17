4c09c89e081e8dc52bd29b848a1b2582e1bc968c
Jiri Stefek <jstefek@redhat.com>
2016-03-17 10:29:43 +0100

    photoalbum: tests: use next button in g+login when displayed (RF-14246)

--- examples/photoalbum/src/test/java/org/richfaces/photoalbum/ftest/webdriver/pages/GPlusLoginPage.java
+++ examples/photoalbum/src/test/java/org/richfaces/photoalbum/ftest/webdriver/pages/GPlusLoginPage.java
@@ -1,17 +1,23 @@
-/**
- * *****************************************************************************
- * JBoss, Home of Professional Open Source Copyright 2010-2014, Red Hat, Inc. and individual contributors by the @authors tag.
- * See the copyright.txt in the distribution for a full listing of individual contributors.
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2016, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- * *****************************************************************************
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.richfaces.photoalbum.ftest.webdriver.pages;
 
@@ -20,6 +26,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.richfaces.fragment.common.ClearType;
 import org.richfaces.fragment.common.TextInputComponentImpl;
+import org.richfaces.fragment.common.Utils;
 
 /**
  * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
@@ -28,15 +35,20 @@ public class GPlusLoginPage implements SocialLoginPage {
 
     @FindBy(css = "input[id$=Email]")
     private TextInputComponentImpl emailInput;
-    @FindBy(css = "input[id$=Passwd]")
-    private TextInputComponentImpl passwordInput;
     @FindBy(css = "input[id$=signIn]")
     private WebElement loginButton;
+    @FindBy(css = "input[id$=next]")
+    private WebElement nextButton;
+    @FindBy(css = "input[id$=Passwd]")
+    private TextInputComponentImpl passwordInput;
 
     @Override
     public void login(String email, String password) {
         Graphene.waitModel().until().element(emailInput.advanced().getInputElement()).is().visible();
         emailInput.advanced().clear(ClearType.DELETE).sendKeys(email);
+        if (Utils.isVisible(nextButton)) {// sometimes the next button is present and needs to be clicked to show the pswd input
+            nextButton.click();
+        }
         Graphene.waitAjax().until().element(passwordInput.advanced().getInputElement()).is().visible();
         passwordInput.advanced().clear(ClearType.DELETE).sendKeys(password);
         loginButton.click();
